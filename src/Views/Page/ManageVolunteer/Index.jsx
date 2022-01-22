94a031b03497cc5e158a5a56da2d7858d9b3370e
Himanshu <himanshuhisar687@gmail.com>
2022-01-22 14:34:49 +0530

    Manage-Volunteer and New-Volunteer pages

--- src/Views/Page/ManageVolunteer/Index.jsx
+++ src/Views/Page/ManageVolunteer/Index.jsx
@@ -8,32 +8,23 @@ import { Link } from "react-router-dom";
 const ManageVolunteer = () => {
   return (
     <>
-      {/* HEADER */}
 
       <HeaderUp />
 
       <div className="container px-4 pt-4">
-        {/* SEARCH BOX AND CARD_DATA */}
 
         <div className="volunteerSearchBox">
           <h3 className="pb-2">Manage Volunteers</h3>
-
-          {/* SEARCH_BOX */}
 
           <div className="searchReceiptVol">
             <i className="fas fa-search searchIconVol"></i>
             <input type="text" placeholder="Search by name, phone" />
           </div>
         </div>
-
-        {/* CARD_DATA */}
-
         <div className="cardReceipts pt-4">
           <ManageRecData shareBtn={false} cardFooter={false} />
         </div>
       </div>
-
-      {/* ADD BUTTON */}
 
       <div>
         <Link to="/NewVolunteer">
@@ -42,8 +33,6 @@ const ManageVolunteer = () => {
           </button>
         </Link>
       </div>
-
-      {/* MENU_BAR */}
 
       <NavBar />
     </>
