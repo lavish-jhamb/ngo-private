a658f8c8096c32a76cdff31a36560e866adce923
Lavish jhamb <lavishjhamb1@gmail.com>
2022-01-26 13:50:00 +0530

    implementing-long-press-event

--- src/Components/Card/Index.jsx
+++ src/Components/Card/Index.jsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Style from "./Index.module.css";
+import { useLongPress } from "../../Utils/useLongPress";
 
 function Card({ donor }) {
+  const [check, setCheck] = useState(false);
+
+  const checkboxHandler = (e) => {
+    setCheck(e.target.checked);
+  };
+
+  const longPressProps = useLongPress({
+    onLongPress: (e) => {
+      const nodes = e.target.closest(".card").children;
+      Array.from(nodes).forEach((node) => {
+        node.children[0].style.display = "block";
+      });
+    },
+
+    onClick: (e) => {
+      const nodes = e.target.closest(".card").children;
+      Array.from(nodes).forEach((node) => {
+        node.children[0].style.display = "none";
+      });
+    },
+  });
+
   return (
-    <div className={Style.cardWrapper}>
-      <div className={Style.cards}>
-        <h6>{donor.name}</h6>
-        <div className={Style.reminderWrapper}>
-          <div className={Style.reminder}>
-            <i className="fa-solid fa-phone"></i>
-            <span>{donor.phone}</span>
-          </div>
-          <button>Send Reminder</button>
-        </div>
+    <div className={`container ${Style.MainContainer}`}>
+      <div className={Style.checkbox}>
+        <input
+          type="checkbox"
+          name="checkbox"
+          onChange={checkboxHandler}
+          value={check}
+        />
       </div>
-      <div className={Style.donationDetails}>
-        <span>Last donation:</span>
-        <span>Rs. {donor.donation}</span>
-        <span>({donor.date})</span>
+      <div {...longPressProps} className={Style.cardWrapper}>
+        <div className={Style.cards}>
+          <h6>{donor.name}</h6>
+          <div className={Style.reminderWrapper}>
+            <div className={Style.reminder}>
+              <i className="fa-solid fa-phone"></i>
+              <span>{donor.phone}</span>
+            </div>
+            <button>Send Reminder</button>
+          </div>
+        </div>
+        <div className={Style.donationDetails}>
+          <span>Last donation:</span>
+          <span>Rs. {donor.donation}</span>
+          <span>({donor.date})</span>
+        </div>
       </div>
     </div>
   );
