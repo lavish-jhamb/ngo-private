5590838861ba020bfda9f346da75d0dd06f05b97
Lavish jhamb <lavishjhamb1@gmail.com>
2022-01-26 19:54:07 +0530

    add-firebase

--- src/Views/Page/Auth/Login/Index.jsx
+++ src/Views/Page/Auth/Login/Index.jsx
@@ -3,8 +3,12 @@ import Style from "./Index.module.css";
 import PrimaryLayout from "../../../Layout/Primary/Main";
 import { useNavigate } from "react-router-dom";
 import { uris } from "../../../../Config/Router/URI";
+import firebaseApp from "../../../../firebase";
+import { getAuth, RecaptchaVerifier,signInWithPhoneNumber } from "firebase/auth";
+
 
 function LoginPage() {
+
   let navigate = useNavigate();
 
   const loginHandler = (e) => {
@@ -12,14 +16,44 @@ function LoginPage() {
     navigate(uris.otp);
   };
 
+  const configureRecaptcha = () => {
+    const auth = getAuth(firebaseApp);
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "sign-in-button",
+      {
+        size: "invisible",
+      },
+      auth
+    );
+  };
+
+  const onSignInSubmit = (e) => {
+    e.preventDefault();
+    configureRecaptcha();
+
+    const phoneNumber = "+91" + 7988065836;
+    const appVerifier = window.recaptchaVerifier;
+
+    const auth = getAuth(firebaseApp);
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        console.log("Otp sent");
+      })
+      .catch((error) => {
+        console.log("Otp not sent");
+      });
+  };
+
   return (
     <PrimaryLayout header="Login">
       <div className={Style.loginWrapper}>
         <main className={Style.loginContainer}>
-          <form className={Style.loginForm}>
+          <div id="sign-in-button"></div>
+          <form onSubmit={onSignInSubmit} className={Style.loginForm}>
             <label>Enter phone number</label>
             <input placeholder="Phone number" type="text" />
-            <button onClick={loginHandler} type="submit">
+            <button id="sign-in-button" type="submit">
               Login
             </button>
           </form>
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvj-goK4M8qq70B34sx0aJD35dHGLum_M",
+  authDomain: "otp-test-a11fa.firebaseapp.com",
+  projectId: "otp-test-a11fa",
+  storageBucket: "otp-test-a11fa.appspot.com",
+  messagingSenderId: "1063872453041",
+  appId: "1:1063872453041:web:fbe68bd3a5b7ce5e3a4590",
+  measurementId: "G-4TW85Y1DMP"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
