29365358d43670e664d97d128afba5f3fc34eb25
Himanshu <himanshuhisar687@gmail.com>
2022-03-26 00:42:36 +0530

    volunteer profile UI and functionality

--- src/Views/Page/Profile/Volunteer/Index.jsx
+++ src/Views/Page/Profile/Volunteer/Index.jsx
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import Style from "./Index.module.css";
 import SecondaryLayout from "../../../Layout/Secondary/Main";
 import { useNavigate } from "react-router-dom";
 import { uris } from "../../../../Config/Router/URI";
+import { useForm } from "react-hook-form";
 
 function VolunteerPage() {
-  const [data, setData] = useState({
-    name: "",
-    email: "",
-    birth: "",
-    interest: "",
-  });
-  const { name, email, birth, interest } = data;
 
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const eventHandler = (name) => (e) => {
-    setData({ ...data, [name]: e.target.value });
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
+  const onSubmit = (data) => {
+    const dateOfBirth = data.dateOfBirth;
+    data.dateOfBirth = dateOfBirth.split("-").reverse().join("-");
+    console.log(data);
     navigate(uris.profileCreated);
   };
 
@@ -30,50 +27,94 @@ function VolunteerPage() {
 
   return (
     <SecondaryLayout title="Volunteer Profile" handler={goBack}>
-      <div className={Style.VolunteerProfile}>
-        <div className={Style.form}>
-          <input
-            type="text"
-            value={name}
-            onChange={eventHandler("name")}
-            placeholder="Name"
-          />
-          <input
-            type="text"
-            value={email}
-            onChange={eventHandler("email")}
-            placeholder="Email"
-          />
-          <input
-            type="date"
-            value={birth}
-            onChange={eventHandler("birth")}
-            placeholder="Date of Birth(optional)"
-          />
-          <input
-            type="text"
-            value={interest}
-            onChange={eventHandler("interest")}
-            placeholder="Social Interest(s)"
-          />
-          <h2>Gender (Optional)</h2>
-          <div className={Style.radioButtons}>
-            <div className={Style.male}>
-              <input type="radio" defaultChecked name="radio" value="option1" />
-              <label htmlFor="male">Male</label>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={Style.VolunteerProfile}>
+          <div className={Style.form}>
+            <div className={Style.volunteerProfileInput}>
+              <input
+                autoComplete="off"
+                type="text"
+                placeholder="Name"
+                {...register("name", {
+                  required: {
+                    value: true,
+                    message: "name is required",
+                  },
+                })}
+              />
+              {errors.name && <p>{errors.name.message}</p>}
             </div>
-            <div className={Style.female}>
-              <input type="radio" name="radio" value="option2" />
-              <label htmlFor="female">Female</label>
+            <div className={Style.volunteerProfileInput}>
+              <input
+                autoComplete="off"
+                type="email"
+                placeholder="Email"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "email is required",
+                  },
+                })}
+              />
+              {errors.email && <p>{errors.email.message}</p>}
+            </div>
+            <div className={Style.volunteerProfileInput}>
+              <input
+                type="text"
+                onFocus={(e) => {
+                  e.currentTarget.type = "date";
+                  e.currentTarget.focus();
+                }}
+                placeholder="Date of Birth"
+                {...register("dateOfBirth", {
+                  required: {
+                    value: true,
+                    message: "dob is required",
+                  },
+                })}
+                onBlur={(e) => {
+                  e.currentTarget.type = "text";
+                  e.currentTarget.blur();
+                }}
+              />
+              {errors.dateOfBirth && <p>{errors.dateOfBirth.message}</p>}
+            </div>
+            <input
+              autoComplete="off"
+              type="text"
+              placeholder="Social Interest(s)"
+              {...register("socialInterest")}
+            />
+            <h2>Gender (Optional)</h2>
+            <div className={Style.radioButtons}>
+              <div className={Style.male}>
+                <input
+                  type="radio"
+                  defaultChecked
+                  value="male"
+                  id="male"
+                  {...register("gender")}
+                />
+                <label htmlFor="male">Male</label>
+              </div>
+              <div className={Style.female}>
+                <input
+                  type="radio"
+                  value="female"
+                  id="female"
+                  {...register("gender")}
+                />
+                <label htmlFor="female">Female</label>
+              </div>
             </div>
           </div>
+          <div className={Style.buttonContainer}>
+            <button className="button" type="submit">
+              Submit
+            </button>
+          </div>
         </div>
-        <div className={Style.buttonContainer}>
-          <button className="button" type="button" onClick={submitHandler}>
-            Submit
-          </button>
-        </div>
-      </div>
+      </form>
     </SecondaryLayout>
   );
 }
